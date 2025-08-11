7fd10790f92a94f96512335b950c41aaff019a46
sachin <mehrasachinr30@gmail.com>
2025-08-11 23:36:32 +0530

    latest-changes

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config()
 
 // Define the MongoDB connection URL and Connction Establish url
-const mongoURL = "mongodb://localhost:27017/hotels"; // Replace 'mydatabase' with your database name
+// const mongoURL = process.env.MONGODB_URL_Local; // Replace 'mydatabase' with your database name
+const mongoURL = process.env.MONGODB_URL 
 
 // Set up MongoDB connection
 mongoose.connect(mongoURL, {
--- index.js
+++ index.js
@@ -240,9 +240,13 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require("dotenv").config()
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 8000;
+
+
 
 app.get("/", (req, res) => {
 res.send(
@@ -258,6 +262,6 @@ const menuRoutes = require("./routes/menuRoutes")
 app.use("/person", personRoutes);
 app.use("/menu", menuRoutes);
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
       console.log("Server is running at 8000");
 });
